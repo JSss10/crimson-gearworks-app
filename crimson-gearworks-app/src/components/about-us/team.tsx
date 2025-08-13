9aa1f061683f82f7977fc0720a0e542b0e4ab85d
Jessica Schneiter <jessica.schneiter@bluewin.ch>
2025-08-13 15:14:57 +0200

    feat(about-us): adjust overall layout and add paragraph with text scroll

--- crimson-gearworks-app/src/components/about-us/team.tsx
+++ crimson-gearworks-app/src/components/about-us/team.tsx
@@ -3,6 +3,9 @@
 import React, { useEffect, useMemo, useRef, useState } from "react";
 import gsap from "gsap";
 import styles from "@/styles/team.module.css";
+import Character from "@/components/text-scroll/character";
+
+const paragraph = "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet."
 
 export type TeamMember = { name: string; img: string };
 
@@ -196,6 +199,7 @@ export default function Team() {
           </div>
         ))}
       </div>
+      <Character paragraph={paragraph} />
     </section>
   );
 }
